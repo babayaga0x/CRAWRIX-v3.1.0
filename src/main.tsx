d45240c885c29f887f668f4bb53416ece034c475
Martin <115161917+babayaga0x@users.noreply.github.com>
2026-03-06 13:29:14 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./i18n";
 
+fetch('https://crawllab-frontend.onrender.com/ping').catch(() => {})
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
